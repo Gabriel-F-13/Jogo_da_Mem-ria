086a727ba5123f3c8e47b6e0650c7643b06813b5
Gabriel Ferreira Barros <ferreiragabriel648@yahoo.com>
2024-01-22 23:42:43 -0300

    add files

---
+++ js/login.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('.loginInput');
+const button = document.querySelector('.loginButton');
+const form = document.querySelector('.loginForm')
+
+
+// Verificar se o usuário digitou algo no campo de login > 2
+const validationInput = ({ target }) => {
+   if(target.value.length > 2) {
+    button.removeAttribute('disable');
+    return;
+   } 
+   
+   button.setAttribute('disable', '');
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('name_Player', input.value);
+    window.location = 'pages/game.html'
+
+
+}
+
+input.addEventListener('input', validationInput);
+form.addEventListener('submit', handleSubmit);
+
